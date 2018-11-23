a8e6c7ca8189ccad994d82b3514dc1920f09218c
Otomatis Gagal <43661631+mbest99@users.noreply.github.com>
2018-11-23 22:48:25 +0700

    Update restart.py

--- restart.py
+++ restart.py
@@ -5,26 +5,9 @@ def mengetik(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(random.random() * 0.4)
+        time.sleep(random.random() * 0.2)
 mengetik('menghubungi Mr Mbest..')
 mengetik('.')
-mengetik('▇▇')
+mengetik('.')
 mengetik('mencoba meminta akses..')
-         
-import random
-import sys
-import time
-def ktik(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(random.random() * 0.2)
-ktik ('mencoba membangunkan')
-ktik('▇▇▇▇▇▇')
-ktik'(▇▇▇▇▇▇▇▇')
-ktik('▇▇▇▇▇▇▇▇▇▇')
-ktik('▇▇▇▇▇▇▇▇▇▇▇▇▇▇')
-ktik('▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇')
-ktik('▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇')
-ktik('.')
-ktik('akses aman !!')
+mengetik ('bodo amat cok !!')
