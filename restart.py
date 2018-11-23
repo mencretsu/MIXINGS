9ccfc2bb22dc3c9fcbd76aa8aebe81dfc5a86c9c
Otomatis Gagal <43661631+mbest99@users.noreply.github.com>
2018-11-23 21:34:27 +0700

    Update and rename ab.py to restart.py

---
+++ restart.py
@@ -0,0 +1,30 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.4)
+mengetik('menghubungi Mr Mbest..')
+mengetik(.)
+mengetik(▇▇')
+mengetik('mencoba meminta akses..')
+         
+import random
+import sys
+import time
+def ktik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+ktik ('mencoba membangunkan')
+ktik('▇▇▇▇▇▇')
+ktik'(▇▇▇▇▇▇▇▇')
+ktik('▇▇▇▇▇▇▇▇▇▇')
+ktik('▇▇▇▇▇▇▇▇▇▇▇▇▇▇')
+ktik('▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇')
+ktik('▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇')
+ktik('.')
+ktik('akses aman !!')
